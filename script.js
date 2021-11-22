b39584b51a690f73ede9aa95396b16ef737088da
Psyh2409 <psyh2409@gmail.com>
2021-11-22 18:18:45 +0200

    clear()=>obj.txt='';

--- script.js
+++ script.js
@@ -1,31 +1,34 @@
-// 5.1 by condition
+// 5 by condition
 strichkBuffer = () => {
     let strng = "";
     return (smthng) => {
-        if (!smthng)
-            return alert("print:\n" + strng);
+        if (!smthng) {
+            return confirm("CLEAR STRICHKA?\n"+strng+"\nOR PUSH CANCEL.")?strng='':strng;
+            }
         strng += (smthng + " ");
     };
 }
 let result = strichkBuffer();
-// 5.1 by discrepant example
+// 5 by example
 makeBuffer = () => {
     let obj = {};
     obj.txt = '';
     obj.add = (smth) => obj.txt += (smth + (isNaN(smth) ? ' ' : ''));
     obj.print = () => obj.txt;
+    obj.clear = () => obj.txt = '';
     return obj;
 }
 let buffer = makeBuffer();
 buffer.add('JavaScript');
-buffer.add('is');
 buffer.add('required');
-buffer.add('to');
 buffer.add('learn');
 console.log(buffer.print());
 
-buffer = makeBuffer();
+buffer = makeBuffer(); // buffer.clear()
 buffer.add('1');
 buffer.add('0');
 buffer.add('1');
+console.log(buffer.print()); // ;)
+
+buffer.clear();
 console.log(buffer.print());
